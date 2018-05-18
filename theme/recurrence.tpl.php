3ceeccd9e61449ea387413de31c937be148e6dba
brm-tangarifec <cristian.tangarife@brm.com.co>
2018-05-18 17:58:36 -0500

    Se quita el hola

--- theme/recurrence.tpl.php
+++ theme/recurrence.tpl.php
@@ -1,6 +1,5 @@
 <?php
 //printVar($variables['carrito']);
-echo 'Hola';
 
 // drupal_add_js(base_path() . path_to_theme() . '/js/libs/jquery.validate.js', array( 'scope' => 'footer', 'weight' => 5 , 'group' => JS_LIBRARY, 'preprocess' => FALSE));
 // drupal_add_js(base_path() . path_to_theme() . '/js/brm.feedback.js', array( 'scope' => 'footer', 'weight' => 6 , 'group' => JS_LIBRARY, 'preprocess' => FALSE));
