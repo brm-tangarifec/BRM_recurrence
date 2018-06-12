255559e5838de4cfdc18472187118778b758c6a8
Cristian Cortés <cristian.cortes@brm.com.co>
2018-06-12 17:55:44 -0500

    template feedback

---
+++ theme/recurrence-feedback.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<div id="container">
+	<div class="row">
+		
+			<div class="col-md-4 col-md-offset-4">
+			<h1 class="text-center">Tu frecuencia ha sido activada</h1>
+		
+			<a href="#" class="btn btn-comprar btn-lg form-submit">Continuar comprando</a>
+		</div>	
+
+	</div>
+</div>
