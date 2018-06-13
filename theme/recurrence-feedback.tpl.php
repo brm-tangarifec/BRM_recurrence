3c9885919127befd234d0714dc86fe0c8ccb09fa
Cristian Cortés <cristian.cortes@brm.com.co>
2018-06-12 19:11:13 -0500

    ajuste form

--- theme/recurrence-feedback.tpl.php
+++ theme/recurrence-feedback.tpl.php
@@ -6,7 +6,7 @@
 	<div class="row">
 		
 			<div class="col-md-4 col-md-offset-4">
-			<h1 class="text-center">Tu frecuencia ha sido activada</h1>
+			<h1 class="text-center text-title-recurrence">Tu frecuencia ha sido activada</h1>
 		
 			<a href="#" class="btn btn-comprar btn-recurrence btn-lg form-submit">Continuar comprando</a>
 		</div>	
