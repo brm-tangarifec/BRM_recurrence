c095b6066c530f8d5962d116fd9af143dab067b1
Cristian Cortés <cristian.cortes@brm.com.co>
2018-06-12 20:11:18 -0500

    ajuste recurrence tpl - css

--- theme/recurrence-feedback.tpl.php
+++ theme/recurrence-feedback.tpl.php
@@ -2,14 +2,12 @@
 
 ?>
 
-<div id="container">
-	<div class="row">
+	<div class=" title row">
 		
-			<div class="col-md-4 col-md-offset-4">
+			<div class="col-md-6 col-md-offset-3">
 			<h1 class="text-center text-title-recurrence">Tu frecuencia ha sido activada</h1>
 		
 			<a href="#" class="btn btn-comprar btn-recurrence btn-lg form-submit">Continuar comprando</a>
 		</div>	
 
 	</div>
-</div>
