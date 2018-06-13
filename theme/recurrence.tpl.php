fd7c911d27925af231eaca24f9a2998964c8573c
brm-tangarifec <cristian.tangarife@brm.com.co>
2018-06-12 19:38:48 -0500

    Se modifica las ciudades

--- theme/recurrence.tpl.php
+++ theme/recurrence.tpl.php
@@ -90,7 +90,7 @@ updateLineShipping($orderRecurrence->order_id,$envio);
 				<div class="form-item form-item-panes-delivery-delivery-city form-type-select">
 				  <label for="edit-panes-delivery-delivery-city"><span class="form-required" title="Este campo es obligatorio.">*</span> Ciudad </label>
 				 <select class="ciudadS form-select form-type-select" id="ciudadS" disabled="disabled"><option val="">--Seleccionar--</option>
-				 	<option val="92">Bogotá</option></select><input type="text" id="edit-panes-delivery-delivery-city" name="panes[delivery][delivery_city]" value="" size="32" maxlength="128" class="form-text">
+				 	<option val="92">Bogotá</option></select><input type="text" id="edit-panes-delivery-delivery-city" name="panes[delivery][delivery_city]" value="92" size="32" maxlength="128" class="form-text hidden">
 				</div>
 				<div class="form-item form-type-select form-item-panes-delivery-delivery-country">
 				  <label for="edit-panes-delivery-delivery-country"><span class="form-required" title="Este campo es obligatorio.">*</span> País </label>
